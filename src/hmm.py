f38720f53601821fb83487b7d4c1df73c0beef0d
iker0610 <ikerprogrammer0610@gmail.com>
2023-10-28 21:57:37 +0200

    HMM: Calculation of the log probabilities in the fit method

--- src/hmm.py
+++ src/hmm.py
@@ -6,13 +6,16 @@ from numpy import ndarray
 
 from dataset_loader import Dataset, DatasetSplit
 
+# Ignore division by zero warnings
+np.seterr(divide='ignore')
+
 
 class HiddenMarkovModel:
     states: ndarray  # States (Q)
     observations: ndarray  # Observations (O)
-    transition_probabilities: ndarray  # Transition Probability Matrix (A)
-    emission_likelihoods: ndarray  # Emission Probability Matrix (B)
-    initial_probability_distribution: ndarray  # Initial Probability Distribution (π)
+    transition_probabilities: ndarray  # Transition Probability Matrix (A); shape: (Q[t_{i-1}] x Q[t_{i}])
+    emission_likelihoods: ndarray  # Emission Probability Matrix (B); shape (Q[ti] x O[wi])
+    initial_probability_distribution: ndarray  # Initial Probability Distribution (π); shape: (Q)
 
     def __init__(
             self,
@@ -23,7 +26,7 @@ class HiddenMarkovModel:
         self.observations = np.asarray(list(dataset.vocabulary))
 
         self.transition_probabilities = np.zeros((len(self.states), len(self.states)))
-        self.emission_likelihoods = np.zeros((len(self.observations), len(self.states)))
+        self.emission_likelihoods = np.zeros((len(self.states), len(self.observations)))
         self.initial_probability_distribution = np.zeros(len(self.states))
 
         # Fit the model to the dataset
@@ -39,14 +42,15 @@ class HiddenMarkovModel:
             # Calculate the transition probabilities and emission likelihoods
             for (token_1, tag_1), (token_2, tag_2) in pairwise(sentence):
                 self.transition_probabilities[self.states == tag_1, self.states == tag_2] += 1
-                self.emission_likelihoods[self.observations == token_1, self.states == tag_1] += 1
+                self.emission_likelihoods[self.states == tag_1, self.observations == token_1] += 1
             else:
                 # Add the last token to the emission likelihoods
-                self.emission_likelihoods[self.observations == sentence[-1][0], self.states == sentence[-1][1]] += 1
+                self.emission_likelihoods[self.states == sentence[-1][1], self.observations == sentence[-1][0]] += 1
 
-
-
-
+        # Calculate log probabilities
+        self.initial_probability_distribution = np.log2(self.initial_probability_distribution) - np.log2(np.sum(self.initial_probability_distribution))
+        self.transition_probabilities = np.log2(self.transition_probabilities) - np.log2(np.sum(self.transition_probabilities, axis=1, keepdims=True))
+        self.emission_likelihoods = np.log2(self.emission_likelihoods) - np.log2(np.sum(self.emission_likelihoods, axis=1, keepdims=True))
 
 
 if __name__ == '__main__':
