2e37557ff73af8001d9fa9256ac006f7c0cd5b22
janir3 <janiiree06@gmail.com>
2023-10-29 18:05:42 +0100

    Evaluation metrics: recall

--- src/evaluation_metrics.py
+++ src/evaluation_metrics.py
@@ -9,9 +9,10 @@ def precision(y_true, y_pred, tag):
     return len(tp) / len(tp_fp)
 
 
-def recall(y_true, y_pred):
-    # TODO
-    return 0.0
+def recall(y_true, y_pred, tag):
+    tp = [g_tag for g_tag, p_tag in zip(y_true, y_pred) if p_tag == p_tag and p_tag == tag]
+    tp_fn = [g_tag for g_tag, p_tag in zip(y_true, y_pred) if g_tag == tag]
+    return len(tp) / len(tp_fn)
 
 
 def f1(y_true, y_pred):
